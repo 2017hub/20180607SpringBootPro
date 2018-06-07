192e7535cfb7bc39742a7094fd18edfdcb4a3546
longyaoyuan <longyaoyuan1987@163.com>
2018-06-07 19:10:20 +0800

    创建一个springboot应用的项目

---
+++ src/main/java/com/springbootPro/SpringbootProMainApplication.java
@@ -0,0 +1,14 @@
+package com.springbootPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @SpringBootApplication 标注主程序，说明是一个springboot应用
+ */
+@SpringBootApplication
+public class SpringbootProMainApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SpringbootProMainApplication.class,args);
+    }
+}
